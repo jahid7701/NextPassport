766296b1ba035a9b8f809ce2c104d77697255114
jahid7701 <jahid128085@gmail.com>
2023-08-19 23:00:05 +0600

    first commit

---
+++ src/app/navber/apply/page.js
@@ -0,0 +1,48 @@
+'use client'
+export default function Apply(){
+   
+    
+    return(
+        
+        <div className="apply">
+        <h1>Step 1: Check availability of e‑Passport in your region</h1>
+    <h1 >Are you Bangladeshi?</h1>
+    <div>
+    <input  type="radio" name="status"/>YES
+    <br/>
+    <input type="radio" name="status" />NO
+    </div>
+    <form className="bangladeshi1_o">
+        <div>
+        <p>Select District your present address</p>
+        <input type="text" />
+        <p> Select the police station nearest to your present address</p>
+        <input type="text"  />
+        </div>
+        <div>
+        <p>Select District your present address</p>
+        <input type="text" />
+        <p> Select the police station nearest to your present address</p>
+        <input type="text"  />
+        </div>
+
+    </form>
+    <form className="bangladeshi_p">
+        <div>
+        <p>Select District your present address</p>
+        <input type="text" />
+        <p> Select the police station nearest to your present address</p>
+        <input type="text"  />
+        </div>
+        <div>
+        <p>Select District your present address</p>
+        <input type="text" />
+        <p> Select the police station nearest to your present address</p>
+        <input type="text"  />
+        </div>
+
+    </form>
+    
+    </div>
+    )
+}
---
+++ src/component/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import "./footer.css";
+export default function Footer() {
+    return (<div className="footer-div">
+        <div className="fahim">
+        <h1>This is Footer Page</h1>
+        <p>@Copyright not allow</p>
+        </div>
+    </div>)
+}
