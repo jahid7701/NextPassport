80a9004b198ea70060812185d941f0f0a28d4a32
jahid7701 <jahid128085@gmail.com>
2023-08-23 21:43:04 +0600

    icon

--- src/app/apply/page.js
+++ src/app/apply/page.js
@@ -1,45 +1,47 @@
 'use client'
 export default function Apply(){
    
+    const positiv=()=>{
+        const hasan=document.querySelector(".bangladeshi1_o");
+       
+        hasan.style.display="block";
+        const hasanin=document.querySelector(".bangladeshi1_p");
+        hasanin.style.display="none";
+
+    }
     
     return(
         
         <div className="apply">
         <h1>Step 1: Check availability of e‑Passport in your region</h1>
     <h1 >Are you Bangladeshi?</h1>
-    <div>
-    <input  type="radio" name="status"/>YES
+    <div className="radiob">
+        <div className="radiobt1">
+    <input  type="radio" name="status"  onClick={positiv} />YES
+    </div>
     <br/>
+    <div>
     <input type="radio" name="status" />NO
     </div>
-    <form className="bangladeshi1_o">
+    </div>
+    <form className="bangladeshi1_p">
         <div>
         <p>Select District your present address</p>
         <input type="text" />
         <p> Select the police station nearest to your present address</p>
         <input type="text"  />
         </div>
-        <div>
-        <p>Select District your present address</p>
-        <input type="text" />
-        <p> Select the police station nearest to your present address</p>
-        <input type="text"  />
-        </div>
+       
 
     </form>
-    <form className="bangladeshi_p">
+    <form className="bangladeshi1_o">
         <div>
-        <p>Select District your present address</p>
+        <p>Select District your present addressBangladesho</p>
         <input type="text" />
         <p> Select the police station nearest to your present address</p>
         <input type="text"  />
         </div>
-        <div>
-        <p>Select District your present address</p>
-        <input type="text" />
-        <p> Select the police station nearest to your present address</p>
-        <input type="text"  />
-        </div>
+    
 
     </form>
     
--- src/component/Card/Cardcomp.js
+++ src/component/Card/Cardcomp.js
@@ -2,7 +2,7 @@ import "./card.css";
 import Link from "next/link";
 export default function Cardcom(props) {
     return (<div className="cardcomp">
-        <div className="cardicon">{props.hicon}</div>
+        <div className="cardicon"><div className="bicon">{props.hicon}</div></div>
         <div className="carddetail">
             <div className="cardcon">
                 <div><h3>{props.name}</h3></div>
