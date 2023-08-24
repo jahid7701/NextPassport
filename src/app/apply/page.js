51d3fe8d5d947f13a34eee71e8aec0de81071165
jahid7701 <jahid128085@gmail.com>
2023-08-24 08:45:07 +0600

    radio

--- src/app/apply/page.js
+++ src/app/apply/page.js
@@ -13,35 +13,41 @@ export default function Apply(){
     return(
         
         <div className="apply">
+            <div className="haji">
         <h1>Step 1: Check availability of e‑Passport in your region</h1>
-    <h1 >Are you Bangladeshi?</h1>
+    <h3 >Are you Bangladeshi?</h3>
+    </div>
+   {/* radio button */}
     <div className="radiob">
         <div className="radiobt1">
-    <input  type="radio" name="status"  onClick={positiv} />YES
+    <input  type="radio" name="status" checked  onClick={positiv} />YES
     </div>
+   
     <br/>
     <div>
     <input type="radio" name="status" />NO
     </div>
     </div>
+    {/* radiobtn */}
     <form className="bangladeshi1_p">
-        <div>
+        <div className="native">
         <p>Select District your present address</p>
         <input type="text" />
-        <p> Select the police station nearest to your present address</p>
+        <p className="fuji"> Select the police station nearest to your present address</p>
         <input type="text"  />
         </div>
-       
+        <button type="submit" className="btn">Continue</button>
 
     </form>
     <form className="bangladeshi1_o">
-        <div>
-        <p>Select District your present addressBangladesho</p>
+        <h2>Select the country of your present address</h2>
+        <div className="foreign">
+        <p className="jahil">Select country</p>
         <input type="text" />
-        <p> Select the police station nearest to your present address</p>
+        <p className="hnn"> Select a Bangladesh Mission</p>
         <input type="text"  />
         </div>
-    
+    <button type="submit" className="btn">Continue</button>
 
     </form>
     
