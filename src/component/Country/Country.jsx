a0857ff97d836323917d53ef51f92f4743c486fc
jahid7701 <jahid128085@gmail.com>
2023-08-29 16:49:24 +0600

    new

--- src/component/Country/Country.jsx
+++ src/component/Country/Country.jsx
@@ -1,23 +1,27 @@
 ' use client'
 import { useState } from 'react'
 import { Combobox } from '@headlessui/react'
-import desh from "@/lib/Countrydata";
-
-// const people = [
-//   'Durward Reynolds',
-//   'Kenton Towne',
-//   'Therese Wunsch',
-//   'Benedict Kessler',
-//   'Katelyn Rohan',
-// ]
+const people = [
+  'Bangladesh',
+  'Nepal',
+  'Pakistan',
+  'South Africa',
+  'England',
+  'UnitedArab Amirat',
+  'Austarlia',
+  'Switzerland',
+  'Finland',
+  'Zimabuwe',
+  'Japan'
+]
 
 export default function Country(){
 const [selectedPerson, setSelectedPerson] = useState("");
 const [query, setQuery] = useState('')
  const filteredPeople =
     query === ''
-      ? desh
-      : desh.filter((person) => {
+      ? people
+      : people.filter((person) => {
           return person.toLowerCase().includes(query.toLowerCase())
         })
 
